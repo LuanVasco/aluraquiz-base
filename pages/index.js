9940c8910622edb7a3c8278df407acb86eec195b
LuanVasco <devluangodoy@gmail.com>
2021-01-31 12:39:39 -0300

    Subindo algumas atualizações do projeto

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,60 @@
+import React from 'react';
 import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-import QuizBackground from '../src/components/QuizBackground';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: 0 auto;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
-    <QuizBackground backgroundImage={db.bg2}>
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Quiz Dota 2</title>
+        <link rel="icon" href="https://image.flaticon.com/icons/png/128/871/871366.png" />
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
-            <Widget.Header>
+          <Widget.Header>
             <h1>Dotinha o melhor moba</h1>
-            </Widget.Header>
+          </Widget.Header>
           <Widget.Content>
-            <p>Será que você realmente sabe jogar dotinha da maneira certa?</p>
+            <form onSubmit={function(event) {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log("Fazendo uma submissão por meio do react");
+            }}> <Input
+                  name="nomeDoUsuario"
+                  onChange={(event) => setName(event.target.value)}
+                  placeholder="Digite seu nome"
+                  value={name}
+                />
+              <Button type="submit" disabled={name.length === 0}>{`Bora jogar ${name}`}</Button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
           <Widget.Content>
-            <h1>Quizes da Galera</h1> 
+            <h1>Quizes da Galera</h1>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
           </Widget.Content>
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/LuanVasco"/>
+      <GitHubCorner projectUrl="https://github.com/LuanVasco" />
     </QuizBackground>
 
   );
--- src/components/QuizBackground/index.js
+++ src/components/QuizBackground/index.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const QuizBackground = styled.div`
   width: 100%;
   background-size: cover;
-  background-position: center;
+  background-position: bottom;
   background-image: url(${({ backgroundImage }) => backgroundImage});
   background-color: ${({ theme }) => theme.colors.mainBg};
   flex: 1;
